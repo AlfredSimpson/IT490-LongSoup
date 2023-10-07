6c131bceed7f94644586201e1f88d596b83ab67d
AlfredSimpson <alfredhsimpson@gmail.com>
2023-10-07 16:55:07 -0400

    removed shell script

--- Broker/Testing/entervenv.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Get the current directory as a variable
-
-$curr_dir = pwd;
-
-# Activate the virtual environment (Only bash support for this)
-
-source $curr_dir/.venv/bin/activate;
