32480b22953a707465351902a6cf52494dcfbb9e
AlfredSimpson <alfredhsimpson@gmail.com>
2023-10-09 23:49:13 -0400

    Full login now goes to db

--- Front/Testing/send.php
+++ Front/Testing/send.php
@@ -7,8 +7,6 @@ require_once('rabbitMQLib.inc');
 // Note, rabbit.ini and tempServer are *currently being used* for testing purposes.
 $client = new rabbitMQClient("rabbit.ini", "tempServer");
 
-
-
 //  This stays, but only bc message needs defined. This takes command line args.
 if (isset($argv[1])) {
     $msg = $argv[1];
@@ -16,17 +14,18 @@ if (isset($argv[1])) {
     $msg = "Testing testing test";
 }
 
+//$useremail = $uname;
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $useremail = $_POST['useremail'] ?? null;
     $password = $_POST['password'] ?? null;
-
+	
     // Existing RabbitMQ code
     $request = array();
     $request['type'] = "Login";
-    $request['username'] = $useremail;
+    $request['useremail'] = $useremail;
     $request['password'] = $password;
-    $request['message'] = "email = " . $useremail . " and password " . $password;
+    $request['message'] = "email = " . $useremail . " and password " . $password . ". and this was uname: ".$_POST;
 
     $response = $client->send_request($request);
     echo JSON_encode($response);
