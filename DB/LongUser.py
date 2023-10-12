5b3260732355fc77448f27ac9d6a9e406debd185
Al Simpson <alfredhsimpson@gmail.com>
2023-10-12 10:11:10 -0400

    Started pseudo code for LongUser

--- DB/LongUser.py
+++ DB/LongUser.py
@@ -2,4 +2,27 @@
 
 
 class LongUser:
+    # Initialize the user
+    def __init__(self, db, userid):
+        # get the user info and store it as a class object
+        pass
+
+    # Getters
+    def getProfile(self, userid):
+        """
+        getProfile is a function that gets the profile of a user.
+        It requires the following parameters:
+        userid: the id of the user to get the profile of
+        """
+        # Should return username, interests, and bio, location, f_name, l_name
+
+        # return all table info for user
+
+        pass
+
+    # Setters - what should we set for the user
+    # Examples: new password, first name, last name, city, etc.
+    def set_user_city(self, userid, location):
+        pass
+
     pass
