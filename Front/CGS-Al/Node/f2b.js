b0d236ae08dd96650e1281dafd23a6646e2e0dc3
AlfredSimpson <alfredhsimpson@gmail.com>
2023-10-15 18:10:17 -0400

    Fix: DBWorker.py had an error in it's error handling. Resolved. Task: Added Node files to front

--- Front/CGS-Al/Node/f2b.js
+++ Front/CGS-Al/Node/f2b.js
@@ -2,27 +2,36 @@
 
 var amqp = require('amqplib/callback_api');
 
-amqp.connect('amqp://192.168.68.65', function(error0, connection) {
-    if (error0) {
-        throw error0;
-    }
-    connection.createChannel(function(error1, channel) {
-        if (error1) {
-            throw error1;
-        }
+const rmqConf = {
+	host: '192.168.68.65',
+	port: 5672,
+	username: 'longsoup',
+	password: 'puosgnol',
+	vhost: 'tempHost'
+};
 
-        var queue = 'testQueue';
-        var msg = 'Hello World!';
+amqp.connect(`amqp://${rmqConf.username}:${rmqConf.password}@${rmqConf.host}:${rmqConf.port}/${encodeURIComponent(rmqConf.vhost)}`, {
+}, function (error0, connection) {
+	if (error0) {
+		throw error0;
+	}
+	connection.createChannel(function(error1, channel) {
+		if (error1) {
+			throw error1;
+		}
 
-        channel.assertQueue(queue, {
-            durable: false
-        });
-        channel.sendToQueue(queue, Buffer.from(msg));
+		var queue = 'tempQueue';
+		var msg = 'Hello World!';
+		
+		channel.assertQueue(queue, {
+			durable: true
+		});
+		channel.sendToQueue(queue, Buffer.from(msg));
 
-        console.log(" [x] Sent %s", msg);
-    });
-    setTimeout(function() {
-        connection.close();
-        process.exit(0);
-    }, 500);
+		console.log("[x] Sent %s", msg);
+	});
+	setTimeout(function() {
+		connection.close();
+		process.exit(0);
+	}, 500);
 });
