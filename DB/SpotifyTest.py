88a08098336fe7110b8cb57020cb21c7d049148c
David Villafranca <djvphil413@gmail.com>
2023-10-29 13:59:26 -0400

    adding test files and queries

--- DB/SpotifyTest.py
+++ DB/SpotifyTest.py
@@ -1,11 +1,21 @@
-import spotipy
+import spotipy 
+from spotipy.oauth2 import SpotifyClientCredentials
+# from spotipy.oauth2 import SpotifyOAuth
+
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="0333477c29da440c828ede4dc2eb6747",
+                                                            client_secret="512ee493749c4959af01f8c32f3cd7d5"))
 
 
-def get_from_spotify():
-    data = spotipy.Spotify()
-    results = data.search(q="artist + Queen", type = "artist")
-    print(results)
+results = sp.search(q='tobymac', limit=20)
+for idx, track in enumerate(results['tracks']['items']):
+    print(idx, track['name'], track['artists'], "/n")
 
-    data1 = spotipy.Spotify()
-    results1 = data1.search(q="artist + Connor Price", type = "artist")
-    print(results1)
+
+# results = sp.search(q='rock', limit=20)
+# for idx, genre in enumerate(results['genre']['items']):
+#     print(idx, genre['name'])
+
+# results = sp.search(q='connor price', limit=20)
+# for idx, track in enumerate(results['tracks']['items']):
+
+#     print(idx, track['name'])
