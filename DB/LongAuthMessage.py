fd0ac003c36d71687b7780fb70dfab264dfc4c86
Al Simpson <alfredhsimpson@gmail.com>
2023-12-14 17:03:41 -0500

    Update LongAuthMessage.py
    Fixed circular logic
    
    Co-Authored-By: JustinTecson <99782837+JustinTecson@users.noreply.github.com>

--- DB/LongAuthMessage.py
+++ DB/LongAuthMessage.py
@@ -3,25 +3,14 @@ from email.mime.text import MIMEText
 import random
 import os
 from dotenv import load_dotenv
-from DBWorker import useremail
 
 # from DBWorker import useremail
 
 load_dotenv()
 
 
-# Test only
-# useremail = "8bitjava5354@gmail.com"
-
-# 2FA Code
-number = random.randint(1000, 9999)
-sentNumber = number
-print(sentNumber)
-
 subject = "CGS Authentication Code: "
-body = "Please enter this code at login: " + str(sentNumber)
 sender = os.getenv("LONG_MAIL")
-recipients = useremail
 password = os.getenv("LONG_MAIL_PASS")
 
 
@@ -32,10 +21,10 @@ def send_email(useremail, subject=subject, sender=sender, password=password):
     msg = MIMEText(body)
     msg["Subject"] = subject
     msg["From"] = sender
-    msg["To"] = recipients
+    msg["To"] = useremail
     with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp_server:
         smtp_server.login(sender, password)
-        smtp_server.sendmail(sender, recipients, msg.as_string())
+        smtp_server.sendmail(sender, useremail, msg.as_string())
     print("Message sent to user for verification!")
     return sentNumber
 
